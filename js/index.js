d277cd1c6416940e39fea3c351b3a8eb202438a9
Divya <gaitondede@gmail.com>
2018-02-14 00:21:32 -0500

    New UI

--- js/index.js
+++ js/index.js
@@ -50,20 +50,22 @@ $(window).on('keydown', function(e) {
 })
 
 var Fake = [
-    'Hi there, I\'m BATMAN and you?',
-    'Do you wanna know My Secret Identity?',
+    'Hi there, I\'m Botman',
+    'Do you want to know my secret identity?',
     'Nice to meet you',
     'How are you?',
     'Not too bad, thanks',
     'What do you do?',
+    'Where are you from?',
     'That\'s awesome',
     'Codepen is a nice place to stay',
-    'I think you\'re a nice person',
+    'You\'re swell',
     'Why do you think that?',
     'Can you explain?',
-    'Anyway I\'ve gotta go now',
-    'It was a pleasure chat with you',
-    'Bye',
+    'Anyway, I\'ve got to go now',
+    'It was a pleasure chatting with you',
+    'See you later alligator, after \'while crocodile',
+    'Can\'t you see you\'re in my way now',
     ':)'
 ]
 
